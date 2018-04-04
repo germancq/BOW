bf0ed823ba884ca2185107f6cdb491cfb9351bcb
germancq <germancq@gmail.com>
2018-04-04 13:40:19 +0200

    testbench.py modified

--- python/testbench.py
+++ python/testbench.py
@@ -7,27 +7,35 @@ import os
 import sys
 import time
 import numpy
+import client_
 
 
 measures = []
+transmission_with_errors = []
 def main():
 	for k in range (0, int(sys.argv[1])):
 		print('execution instance %i',k)
 		start = time.time()
 		if(k%2 == 0):
-			os.system('python3 client_.py pAA.HEX')
+			#os.system('python3 client_.py pAA.HEX')
+			client_.maintask('pAA.HEX')
 			print ('test A')
 		else:
-			os.system('python3 client_.py pBB.HEX')
+			#os.system('python3 client_.py pBB.HEX')
+			client_.maintask('pBB.HEX')
 			print ('test B')
+
+		if len(client_.error_packets) > 0:
+			transmission_with_errors.append(k)
+
 		end = time.time()
 		total = end - start
 		print('time execution = %i',total)
 		measures.append(total)
 		print('average time is %i',numpy.average(measures))
-		time.sleep(4)
+		time.sleep(1)
 
-
+	print(transmission_with_errors)
 
 
 if __name__ == "__main__":
